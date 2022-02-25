bd1958ea2e033d300cda6f4f16691665b198caaa
chaoyi <czha5168@uni.sydney.edu.au>
2022-02-25 14:58:02 +1100

    update - get_var_name

--- demo.py
+++ demo.py
@@ -6,11 +6,26 @@ def demo():
     nepochs = 5
 
     for epoch_id in range(nepochs):
-        print(f'[{epoch_id}/{nepochs}] training loss: {np.random.rand()}')
+        loss1 = np.random.rand()
+        loss2 = np.random.rand()
 
-        # two dprint usages
+        # dprint usage1: check tensor size
         tensorA = torch.rand(20, 5)
+        dprint(tensorA.size())
+
+        # dprint usage2: check variable-value
         varB = np.random.rand()
-        dprint({"tensorA.size()": tensorA.size(), "varB":varB})
+        dprint(varB)
+
+        # dprint usage3: monitor multiple things with inline (by passing them as a dict)
+        test_dict = {"tensorA.size()": tensorA.size(), "varB":varB}
+        dprint(test_dict)
+
+        # dpring usage4: monitor within execution (print-and-return)
+        #loss_total = loss1 + loss2
+        loss_total = loss1 + dprint(loss2)
+
+        # normal print during execution
+        print(f'[{epoch_id}/{nepochs}] training loss: {loss_total}')
 if __name__ == '__main__':
     demo()
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import inspect
-def dprint(var_dict, color=True):
+import tokenize
+import token
+import io
+
+def dprint(var_value, color=True):
 
     #debug_print
     BOLD = '\033[1m'
@@ -22,6 +26,42 @@ def dprint(var_dict, color=True):
     caller_funcname = calframe[1][3]
     caller_lineno = calframe[1][2]
 
-    for var_name, var_value in var_dict.items():
-        print('{}[{}-{}()-L{}] {}{}{} = {}{}'.format(START, caller_filename, caller_funcname, caller_lineno, MIDDLE1, var_name, MIDDLE2, var_value, END.replace(COLOR, '')))
+    def _format_expression(code_context):
+        # borrowed from https://github.com/pradyunsg/dprint/blob/de8ed1ad75412d710d31f71e5b477826a92a0005/dprint/_impl.py#L106 to derive the var_name
+        if not code_context:
+            return ""
 
+        line = code_context[0]
+
+        # Tokenize the line
+        token_list = tokenize.tokenize(io.BytesIO(line.encode('utf-8')).readline)
+
+        # Determine the start and end of expression
+        start = None
+        end = None
+        level = 0  # because nesting
+        for tok in token_list:
+            # Looking for the start of string.
+            if start is None:
+                if tok.type == token.NAME and tok.string == "dprint":
+                    start = tok.start[1]  # we get the proper value later.
+                continue
+            if end is None:
+                if tok.type != token.OP:
+                    continue
+
+                if tok.string == "(":
+                    if level == 0:  # opening parens
+                        start = tok.end[1]
+                    level += 1
+                elif tok.string == ")":
+                    level -= 1
+                    if level == 0:  # closing parens
+                        end = tok.start[1]
+                        # This is fine since we don't need more information
+                        break
+        return line[start:end]
+
+    var_name = _format_expression(inspect.stack()[1].code_context)
+    print('{}[{}-{}()-L{}] {}{}{} -> {}{}'.format(START, caller_filename, caller_funcname, caller_lineno, MIDDLE1, var_name, MIDDLE2, var_value, END.replace(COLOR, '')))
+    return var_value
